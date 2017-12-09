4706dd1b115b28c9942e2653fce8a671bd30bf6c
Masaki Ozawa <ozamasa123@gmail.com>
2017-12-09 14:15:17 +0900

    リファクタ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,39 +1,38 @@
 class LinebotController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
+  require "line/bot"
 
   # callbackアクションのCSRFトークン認証を無効
-  protect_from_forgery :except => [:callback]
+  protect_from_forgery except: [:callback]
 
   def client
-    @client ||= Line::Bot::Client.new { |config|
+    @client ||= Line::Bot::Client.new do |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
       config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
+    end
   end
 
   def callback
     body = request.body.read
 
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    signature = request.env["HTTP_X_LINE_SIGNATURE"]
     unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
+      error 400 do "Bad Request" end
     end
 
     events = client.parse_events_from(body)
 
-    events.each { |event|
+    events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
-            type: 'text',
-            text: event.message['text']
+            type: "text",
+            text: event.message["text"]
           }
-          client.reply_message(event['replyToken'], message)
+          client.reply_message(event["replyToken"], message)
         end
       end
-    }
 
     "OK"
   end
