af708819a242e8e59def5158b99124fef17702a6
Masaki Ozawa <ozamasa123@gmail.com>
2017-12-09 14:22:45 +0900

    リファクタ

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,15 +1,6 @@
 class LinebotController < ApplicationController
   require 'line/bot'  # gem 'line-bot-api'
-
-  # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
-
-  def client
-    @client ||= Line::Bot::Client.new do |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    end
-  end
 
   def callback
     body = request.body.read
@@ -20,7 +11,6 @@ class LinebotController < ApplicationController
     end
 
     events = client.parse_events_from(body)
-
     events.each do |event|
       case event
       when Line::Bot::Event::Message
@@ -34,7 +24,14 @@ class LinebotController < ApplicationController
         end
       end
     end
+    head :ok
+  end
 
-    "OK"
+private
+  def client
+    @client ||= Line::Bot::Client.new do |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    end
   end
 end
