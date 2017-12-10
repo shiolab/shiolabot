8c5cc353a9f5d91b73b22c053b778094eb532e5e
Masaki Ozawa <ozamasa123@gmail.com>
2017-12-10 15:52:26 +0900

    change

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -16,7 +16,7 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          image_url = "https://s3.shiolab.com/camera/sip/2017/12/10/2017-12-10_1540.jpg"
+          image_url = "https://s3.shiolab.com/camera/sip/2017/12/10/2017-12-10_1540-thumbnail.jpeg"
           message = {
             tyep: 'image',
             originalContentUrl: image_url,
