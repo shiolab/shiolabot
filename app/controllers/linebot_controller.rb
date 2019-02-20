411da9d1fddc3610424e08ebe5abc0868db5aeea
Masaki Ozawa <ozamasa@shiolab.com>
2019-02-20 22:32:27 +0900

    cosme

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class LinebotController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
-  protect_from_forgery :except => [:callback]
+  require 'line/bot'
+  protect_from_forgery except: [:callback]
 
   def callback
     body = request.body.read
@@ -21,17 +23,22 @@ class LinebotController < ApplicationController
             text: event.message['text']
           }
           client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open('content')
+          tf.write(response.body)
         end
       end
     end
     head :ok
   end
 
-private
+  private
+
   def client
     @client ||= Line::Bot::Client.new do |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_secret = ENV['LINE_CHANNEL_SECRET']
+      config.channel_token = ENV['LINE_CHANNEL_TOKEN']
     end
   end
 end
