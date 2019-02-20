3e4a4c6f1c236265d54a8c6a675d99fdb10c38f3
Masaki Ozawa <ozamasa@shiolab.com>
2019-02-20 22:46:01 +0900

    cosme

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -2,14 +2,14 @@
 
 class LinebotController < ApplicationController
   require 'line/bot'
-  protect_from_forgery except: [:callback]
+  protect_from_forgery except: :callback
 
   def callback
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
+      error :bad_request do 'Bad Request' end
     end
 
     events = client.parse_events_from(body)
@@ -18,16 +18,15 @@ class LinebotController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+          message_text = event.message['text']
         when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
-          response = client.get_message_content(event.message['id'])
-          tf = Tempfile.open('content')
-          tf.write(response.body)
+          message_text = event.message['text']
         end
+        message = {
+          type: 'text',
+          text: message_text
+        }
+        client.reply_message(event['replyToken'], message)
       end
     end
     head :ok
